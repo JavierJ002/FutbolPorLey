ac0ee6be66336a5c521f0331497e5fe77ced5956
JavierJ002 <javierjimenezjq98@gmail.com>
2025-04-07 09:28:25 -0500

    added config for quick seasons setup

---
+++ config/driver_setup.py
@@ -0,0 +1,14 @@
+USER_AGENTS = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.5.1 Safari/605.1.15",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
+]
+_BASE_SOFASCORE_URL = "https://www.sofascore.com/"
+
+# --- Default values (Consider making these configurable or extracting from API if possible) ---
+_DEFAULT_TOURNAMENT_ID = 8
+_DEFAULT_TOURNAMENT_NAME = "LaLiga"
+_DEFAULT_TOURNAMENT_COUNTRY = "Spain" 
+_DEFAULT_SEASON_ID = 37223
+_DEFAULT_SEASON_NAME = "2021/2022" 
